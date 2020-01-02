357dd999714ea0a2631720c59a02facad1e24d07
Malik Kilgore <malik199228@gmail.com>
2020-01-02 03:30:32 -0500

    Implemented ability to color in squares. Created different color button selectors. Basic functionality of Etch-a-Sketch is complete.

--- Etch.js
+++ Etch.js
@@ -1,11 +1,12 @@
-let gridContainer = document.querySelector('.grid-container');
+const gridContainer = document.querySelector('.grid-container');
+let selectedColor = 'black'
 
+//v Needs updated later v
 function defaultGrid() {
     for (let i = 0; i < 256; i++) {
 
         let gridSquare = document.createElement('div');
         gridSquare.className = 'grid-square';
-        //removed gridsquare width/height.
         gridContainer.style.gridTemplateColumns = 'repeat(16, 1fr)';
         gridContainer.style.gridTemplateRows = 'repeat(16, 1fr)';
     
@@ -20,7 +21,6 @@ function defaultGrid() {
 };
 defaultGrid()
 
-
 function eraseGrid() {
     while (gridContainer.firstChild) {
         gridContainer.removeChild(gridContainer.firstChild)
@@ -29,7 +29,7 @@ function eraseGrid() {
 
 function createGrid() {
 
-    let userInput = prompt('How many squares per side do you want to use? (16-64)', '24')
+    let userInput = prompt('How many squares per side do you want to use? (1-100)', '24')
     let size = parseInt(userInput)
     let dimensions = size * size
     gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -39,15 +39,52 @@ function createGrid() {
         
             let gridSquare = document.createElement('div');
             gridSquare.className = 'grid-square';
-            gridSquare.style.width = `repeat(${size}, 1fr)`;
-            gridSquare.style.height = `repeat(${size}, 1fr)`;
+            let coloredIn = false
 
-
-            gridSquare.addEventListener("mouseover", function() {
-                gridSquare.style.backgroundColor = 'grey';
+            gridSquare.addEventListener("mouseover", function(e) {
+                
+                switch (coloredIn) {
+                    case true:
+                        break;
+                    case false:
+                        return gridSquare.style.backgroundColor = 'grey';
+                }
             });
-            gridSquare.addEventListener("mouseout", function() {
-                gridSquare.style.backgroundColor = 'white';
+
+            gridSquare.addEventListener("mouseout", function(e) {
+                
+                switch (coloredIn) {
+                    case true:
+                        break;
+                    case false:
+                        return gridSquare.style.backgroundColor = 'initial';
+                }
+            });
+            
+            gridSquare.addEventListener("mousedown", function(e) {
+               
+                switch(selectedColor) {
+
+                    case 'black':
+                        gridSquare.style.backgroundColor = 'black';
+                        break;
+            
+                    case 'red':
+                        gridSquare.style.backgroundColor = 'red';
+                        break;
+            
+                    case 'blue':
+                        gridSquare.style.backgroundColor = 'blue';
+                        break;
+            
+                    case 'green':
+                        gridSquare.style.backgroundColor = 'green';
+                        break;
+                
+                };
+
+                return coloredIn = true
+
             });
 
             gridContainer.appendChild(gridSquare)
@@ -56,18 +93,39 @@ function createGrid() {
 };
 
 
-//Clear button
-let clearButton = document.querySelector('.clear')
-clearButton.addEventListener('click', function() {
-
-
+let redButton = document.querySelector('#red')
+redButton.addEventListener('click', function(e) {
+    console.log('current color is red')
+    return selectedColor = 'red'
 });
 
-//Resize button
-let resizeButton = document.querySelector('.resize')
-resizeButton.addEventListener('click', function() {
+let blueButton = document.querySelector('#blue')
+blueButton.addEventListener('click', function(e) {
+    console.log('current color is blue')
+    return selectedColor = 'blue'
+});
 
+let greenButton = document.querySelector('#green')
+greenButton.addEventListener('click', function(e) {
+    console.log('current color is green')
+    return selectedColor = 'green'
+});
+
+let blackButton = document.querySelector('#black')
+blackButton.addEventListener('click', function(e) {
+    console.log('current color is black')
+    return selectedColor = 'black'
+});
+
+
+let clearButton = document.querySelector('.clear')
+clearButton.addEventListener('click', function(e) {
+let erase = document.querySelectorAll('.grid-square')
+erase.style.backgroundColor ='white';
+});
+
+let resizeButton = document.querySelector('.resize')
+resizeButton.addEventListener('click', function(e) {
 eraseGrid()
 createGrid()
-
 });
