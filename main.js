66bb81390d4a1c718e9608dcbcb0bfc5b4b2194f
Malik Kilgore <malik199228@gmail.com>
2020-11-13 21:37:28 -0500

    Updated styling. Made it so number greater than 101 cannot be entered when resizing grid.

--- main.js
+++ main.js
@@ -83,82 +83,87 @@ function createGrid() {
 
     let userInput = prompt('How many squares per side do you want to use? (1-100)', '24')
     let size = parseInt(userInput)
-    let dimensions = size * size
-    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-
-        for (let i = 0; i < dimensions; i++) {
-        
-            let gridSquare = document.createElement('div');
-            gridSquare.className = 'grid-square';
-            let coloredIn = false
+    if (size >= 101){
+        alert('Please enter a value of 100 or lower. Setting to default layout.')
+        defaultGrid()
+    } else {
+        let dimensions = size * size
+        gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+        gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    
+            for (let i = 0; i < dimensions; i++) {
             
-
-            gridSquare.addEventListener("mouseover", function(e) {
+                let gridSquare = document.createElement('div');
+                gridSquare.className = 'grid-square';
+                let coloredIn = false
                 
-                switch (coloredIn) {
-                    case true:
-                        break;
-                    case false:
-                        return gridSquare.style.backgroundColor = 'grey';
-                }
-            });
-
-            gridSquare.addEventListener("mouseout", function(e) {
-                
-                switch (coloredIn) {
-                    case true:
-                        break;
-                    case false:
-                        return gridSquare.style.backgroundColor = 'initial';
-                }
-            });
-            
-            gridSquare.addEventListener("mousedown", function(e) {
-               
-                mousePressed = true
-
-                if (mousePressed = true) {
-                    switch(selectedColor) {
-
-                        case 'black':
-                            gridSquare.style.backgroundColor = 'black';
-                            return coloredIn = true
-                        
-            
-                        case 'red':
-                            gridSquare.style.backgroundColor = 'red';
-                            return coloredIn = true
-                        
-            
-                        case 'blue':
-                            gridSquare.style.backgroundColor = 'blue';
-                            return coloredIn = true
-                        
-            
-                        case 'green':
-                            gridSquare.style.backgroundColor = 'green';
-                            return coloredIn = true
-
-                        case 'white':
-                            gridSquare.style.backgroundColor = 'white';
-                            return coloredIn = true
-                
-                    }} else {
-                        return;
+    
+                gridSquare.addEventListener("mouseover", function(e) {
+                    
+                    switch (coloredIn) {
+                        case true:
+                            break;
+                        case false:
+                            return gridSquare.style.backgroundColor = 'grey';
                     }
-
-            });
-
-            gridSquare.addEventListener("mouseup", function(e) {
-                mousePressed = false
-            });
-
-            gridContainer.appendChild(gridSquare)
-        };
+                });
+    
+                gridSquare.addEventListener("mouseout", function(e) {
+                    
+                    switch (coloredIn) {
+                        case true:
+                            break;
+                        case false:
+                            return gridSquare.style.backgroundColor = 'initial';
+                    }
+                });
+                
+                gridSquare.addEventListener("mousedown", function(e) {
+                   
+                    mousePressed = true
+    
+                    if (mousePressed = true) {
+                        switch(selectedColor) {
+    
+                            case 'black':
+                                gridSquare.style.backgroundColor = 'black';
+                                return coloredIn = true
+                            
+                
+                            case 'red':
+                                gridSquare.style.backgroundColor = 'red';
+                                return coloredIn = true
+                            
+                
+                            case 'blue':
+                                gridSquare.style.backgroundColor = 'blue';
+                                return coloredIn = true
+                            
+                
+                            case 'green':
+                                gridSquare.style.backgroundColor = 'green';
+                                return coloredIn = true
+    
+                            case 'white':
+                                gridSquare.style.backgroundColor = 'white';
+                                return coloredIn = true
+                    
+                        }} else {
+                            return;
+                        }
+    
+                });
+    
+                gridSquare.addEventListener("mouseup", function(e) {
+                    mousePressed = false
+                });
+    
+                gridContainer.appendChild(gridSquare)
+            };
+    }
+    
     
 };
-
 
 let redButton = document.querySelector('#red')
 redButton.addEventListener('click', function(e) {
